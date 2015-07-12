0fbf71ed0f40120201e4b14086d80a28ff309404
Christian Martin <christian@topmartin.com>
2015-07-12 08:34:04 +0200

    Added tests

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # angular-inject
 A angular decorator for angular dependency injection.
+
+## Install
+    $ npm install --save angular-inject 
  
 ## Usage
-
+    // ES2016 style
     import {inject} from 'angular-inject'
+
+    // CommonJS style
+    let inject = require('angular-inject').inject;
 
 
     @inject( ['$base64'] )
---
+++ test/inject.test.js
@@ -0,0 +1,16 @@
+var assert = require("assert");
+var inject = require('../dist').inject;
+var _ = require('lodash');
+
+describe('Inject', function() {
+    it('should add inject property on class', function () {
+      var TestClass = function() {
+        this.foo = 'bar'
+      };
+
+      var InjectedClass = inject(['abc'])(TestClass);
+      var instance = new InjectedClass();
+      assert.equal(true, _.has(instance, 'abc'));
+      assert.equal('abc', InjectedClass.$inject[0]);
+  });
+});
